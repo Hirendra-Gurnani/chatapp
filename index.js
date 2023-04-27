73e9d86c03b952e76a9153b2d8c29bbac54ab105
Hirendra-Gurnani <92821585+Hirendra-Gurnani@users.noreply.github.com>
2023-04-27 18:09:56 +0530

    static access on mobile device in same network

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3700;
+var port = process.env.PORT || 80;
 var cons = require("consolidate");
 
 // Set view of '/' end point
