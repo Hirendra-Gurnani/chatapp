77c85a2701cbf5cd8dbb2cf33c141eb04bf645c8
Hirendra-Gurnani <92821585+Hirendra-Gurnani@users.noreply.github.com>
2023-04-27 17:33:55 +0530

    added css for the app and also configured view engine

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3700;
+var cons = require("consolidate");
 
 // Set view of '/' end point
-app.set('views', __dirname + '/views');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-    res.render("page");
+// app.set('views', __dirname + '/views');
+// app.set('view engine', "html");
+// app.engine('html', require('html').__express);
+app.engine("html", cons.swig);
+app.set("views", __dirname + "/views");
+app.set("view engine", "html");
+app.get("/", function (req, res) {
+  res.render("page");
 });
 
 // use our puclic/chat.js file as listener
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 // Set port
 var midPort = app.listen(port, function () {
-    console.log('Node.js listening on port ' + port);
-})
+  console.log("Node.js listening on port " + port);
+});
 
-var io = require('socket.io').listen(midPort);
+var io = require("socket.io").listen(midPort);
 // set up socket connection
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'Welcome to the Real Time Web Chat' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+io.sockets.on("connection", function (socket) {
+  socket.emit("message", { message: "Welcome to the Real Time Web Chat" });
+  socket.on("send", function (data) {
+    io.sockets.emit("message", data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://localhost:3700');
+    var socket = io.connect('http://localhost:3700' , 'http://192.168.110.140:3700');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
